bd0ead3902e38d83d0f46e373cb1da1d422c50fa
Vadym Piddubnyi <leegrad@ukr.net>
2025-04-27 19:15:21 -0500

    Switch to PostgreSQL database

--- airport_api_service/settings.py
+++ airport_api_service/settings.py
@@ -73,8 +73,11 @@ WSGI_APPLICATION = "airport_api_service.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
     }
 }
 
