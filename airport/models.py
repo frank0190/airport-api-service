bd980165263a0bd2ffe6adc30db94a2216c9c2bf
Vadym Piddubnyi <leegrad@ukr.net>
2025-04-30 14:40:57 -0500

    Implement movie_image_file_path function & Add image field for Airplane & Airport models

--- airport/models.py
+++ airport/models.py
@@ -1,6 +1,9 @@
+import os
+import uuid
 from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.text import slugify
 
 
 class AirplaneType(models.Model):
@@ -10,6 +13,14 @@ class AirplaneType(models.Model):
         return self.name
 
 
+def movie_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.name)}-{uuid.uuid4()}{extension}"
+    dirname = f"{slugify(type(instance).__name__)}s/"
+
+    return os.path.join("uploads/", dirname, filename)
+
+
 class Airplane(models.Model):
     name = models.CharField(max_length=255)
     rows = models.IntegerField()
@@ -17,6 +28,7 @@ class Airplane(models.Model):
     airplane_type = models.ForeignKey(
         AirplaneType, on_delete=models.CASCADE, related_name="airplanes"
     )
+    image = models.ImageField(null=True, upload_to=movie_image_file_path)
 
     @property
     def capacity(self) -> int:
@@ -58,6 +70,7 @@ class City(models.Model):
 class Airport(models.Model):
     name = models.CharField(max_length=255)
     closest_big_city = models.ForeignKey(City, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, upload_to=movie_image_file_path)
 
     def __str__(self):
         return f"{self.name} ({self.closest_big_city})"
