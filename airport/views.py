41781837e596885b1efbc1c9e33a546192269bc8
Vadym Piddubnyi <leegrad@ukr.net>
2025-04-29 22:35:22 -0500

    Implement AirplaneViewSet

--- airport/views.py
+++ airport/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, mixins
-from airport.models import AirplaneType
-from airport.serializers import AirplaneTypeSerializer
+from airport.models import AirplaneType, Airplane
+from airport.serializers import AirplaneTypeSerializer, AirplaneSerializer
 
 
 class AirplaneTypeViewSet(
@@ -10,3 +10,12 @@ class AirplaneTypeViewSet(
 ):
     queryset = AirplaneType.objects.all()
     serializer_class = AirplaneTypeSerializer
+
+
+class AirplaneViewSet(
+    viewsets.GenericViewSet,
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin
+):
+    queryset = Airplane.objects.all()
+    serializer_class = AirplaneSerializer
