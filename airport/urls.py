9382fd6a53f6edee299650c72a9f7f24dbf1d0c2
Vadym Piddubnyi <leegrad@ukr.net>
2025-04-29 22:38:01 -0500

    Register AirplaneViewSet

--- airport/urls.py
+++ airport/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
-from airport.views import AirplaneTypeViewSet
-
+from airport.views import AirplaneTypeViewSet, AirplaneViewSet
 
 router = routers.DefaultRouter()
 
 router.register("airplane_types", AirplaneTypeViewSet)
+router.register("airplanes", AirplaneViewSet)
 
 urlpatterns = [path("", include(router.urls))]
 
